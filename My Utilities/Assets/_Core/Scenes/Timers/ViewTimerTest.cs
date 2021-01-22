5fa8c0c42b7337957983cb10fe69ebd9cbadfc39
Aaron Rod <aarone.rodriguez.trujillo@gmail.com>
2021-01-22 02:59:28 -0500

    Escenas de ejemplos para: -Timers. -UI y MainExecution. -Input y Tweens

---
+++ My Utilities/Assets/_Core/Scenes/Timers/ViewTimerTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rod.Utilities.Timers;
+using TMPro;
+using UnityEngine.UI;
+public class ViewTimerTest : MonoBehaviour
+{
+    [SerializeField]
+    TimersConnector timersConnector;
+    [SerializeField]
+    TextMeshProUGUI txtTimer;
+    [SerializeField]
+    Image timerProgress;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timersConnector.ControllerAssigned += (controller) =>
+        {
+            controller.TimerAdded += OnTimerAdded;
+        };
+    }
+
+    void OnTimerAdded(Timer timer)
+    {
+        timer.Updated += (updateArgs) =>
+        {
+            txtTimer.text= string.Format("{0} / {1}", updateArgs.elapsedInSeconds, updateArgs.durationInSeconds);
+            timerProgress.fillAmount = (float)(updateArgs.elapsedInSeconds / updateArgs.durationInSeconds);
+        };
+    }
+
+
+}
